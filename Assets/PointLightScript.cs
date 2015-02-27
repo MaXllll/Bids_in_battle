692204855552a6f37fca24fdfce6392a73f6dfcb
Gval <goulmy.val@gmail.com>
2015-02-27 14:19:52 +0100

    Commit

---
+++ Assets/PointLightScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointLightScript : MonoBehaviour {
+
+	private Light light;
+
+	bool intensityUp = false;
+
+	bool intensityDown = true;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(intensityDown)
+		{
+			light.intensity -= 1.0f;
+		} else {
+			light.intensity += 1.0f;
+		}
+		 
+	}
+}
